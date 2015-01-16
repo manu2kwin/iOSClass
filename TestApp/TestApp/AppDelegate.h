39cae0e339137d08fe36cb0370bbd240281d025d
vivianaranha <vivianaranha@gmail.com>
2015-01-16 12:46:47 -0500

    First Draft

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Vivian Aranha on 1/16/15.
+//  Copyright (c) 2015 Vivian Aranha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Vivian Aranha on 1/16/15.
+//  Copyright (c) 2015 Vivian Aranha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
